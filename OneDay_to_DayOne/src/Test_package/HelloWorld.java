f53ad3d09a4f2d53e81d3aef1205227e27bcd266
shubham1edu <shubhamtiwari.edu@gmail.com>
2023-07-29 01:36:48 +0530

    First Commit

---
+++ OneDay_to_DayOne/src/Test_package/HelloWorld.java
@@ -0,0 +1,11 @@
+package Test_package;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi shubham, you can do it ");
+
+	}
+
+}
